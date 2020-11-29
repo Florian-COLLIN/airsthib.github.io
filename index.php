9333cfec5a4dedebf6b29b3df9606511a5391d0a
AirSThib <75171564+AirSThib@users.noreply.github.com>
2020-11-29 11:26:16 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>AirSThib</title>
+  </head>
+  <body>
+    <h1>Hey ! I'm AirSThib !</h1>
+    <p>I'm AirSThib, a French 14-year-old student in an high school near Toulouse. I love programming (and cycling), and I code with HTML, CSS, PHP, MySQL, JavaScript, Python, C+++, C, and others.</p>
+    <p>On Wikimedia, I'm AirSThib too, and I'm an administrator of <a href="fr.wikinews.org">Wikinews</a>.</p>
+  </body>
+</html>
