efc4e3a5aa42f1569b7102fcb4f15bfe9b5a9977
AirSThib <75171564+AirSThib@users.noreply.github.com>
2020-11-30 14:33:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+btnTop = document.getElementById("btnTop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btnTop.style.display = "block";
+  } else {
+    btnTop.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
