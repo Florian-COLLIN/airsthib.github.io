4684c5a26cb9e904febf26ef97204dbe2374fdf5
AirSThib <75171564+AirSThib@users.noreply.github.com>
2020-12-14 08:48:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -49,8 +49,8 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("countdown-tdf").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("countdown-tdf").innerHTML = "D-" + days + " " + hours + ":"
+  + minutes + ":" + seconds;
 
   if (distance < 0) {
     clearInterval(x);
